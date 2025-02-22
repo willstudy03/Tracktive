2d1de40f164c63efdd549fd87047eea18af60bc5
William Theo <williamtheo0711@gmail.com>
2025-02-22 19:54:37 +0800

    Project Initialization

---
+++ src/main/java/com/tracktive/tracktive/TracktiveApplication.java
@@ -0,0 +1,14 @@
+package com.tracktive.tracktive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracktiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracktiveApplication.class, args);
+		System.out.println("Testing");
+	}
+
+}
---
+++ src/test/java/com/tracktive/tracktive/TracktiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracktive.tracktive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracktiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
