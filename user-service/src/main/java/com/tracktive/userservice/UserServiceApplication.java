8532fd0e0bdff83d95348b21a66db45d7d86dc5d
William Theo <williamtheo0711@gmail.com>
2025-02-24 15:28:43 +0800

    User Service Initialization

---
+++ user-service/src/main/java/com/tracktive/userservice/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tracktive.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+        System.out.println("User Service Running");
+    }
+}
