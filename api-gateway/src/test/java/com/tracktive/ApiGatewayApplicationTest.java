ae2ce5b100cba6ac7ecc809fce6132060b7296d5
William Theo <williamtheo0711@gmail.com>
2025-04-22 11:57:38 +0800

    Dockerize api-gateway

--- api-gateway/src/test/java/com/tracktive/ApiGatewayApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.tracktive;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class ApiGatewayApplicationTest {
-
-}
