d19b58165c3e1041a3a75cf8905af8ae5766a35a
William Theo <williamtheo0711@gmail.com>
2025-05-17 19:55:49 +0800

    Auth Service: CORS Configuration

--- auth-service/src/main/java/com/tracktive/authservice/config/SecurityConfig.java
+++ auth-service/src/main/java/com/tracktive/authservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.tracktive.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,8 +19,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults()) // Enable CORS with default config
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
 
         return http.build();
     }
